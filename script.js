6c03d7580855facbb9705ca2eb9331349a449dbd
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-04-04 14:10:44 -0700

    Complete basic functionality of the dynamic grid

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+// Execute the JS after the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
   const etchDiv = document.querySelector(".etch");
+  const newGridBtn = document.querySelector("#new-grid-btn");
 
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-      const div = document.createElement("div");
-      div.classList.add("square");
-      etchDiv.appendChild(div);
-      div.addEventListener("mouseover", function () {
-        this.style.backgroundColor = "black";
-      });
+  newGridBtn.addEventListener("click", function () {
+    let squaresPerSide = prompt("Enter number of squares per side (max 100):");
+    squaresPerSide = Math.min(Math.max(parseInt(squaresPerSide), 1), 100);
+    createGrid(squaresPerSide);
+  });
+
+  function createGrid(squaresPerSide) {
+    // Set CSS variable for square size
+    etchDiv.style.setProperty("--squares-per-side", squaresPerSide);
+
+    // Remove existing squares
+    while (etchDiv.firstChild) {
+      etchDiv.removeChild(etchDiv.firstChild);
+    }
+
+    // Add new squares with dynamic size
+    for (let i = 0; i < squaresPerSide; i++) {
+      for (let j = 0; j < squaresPerSide; j++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        etchDiv.appendChild(div);
+        // Add hover event listener to each square
+        div.addEventListener("mouseover", function () {
+          this.style.backgroundColor = "black";
+        });
+      }
     }
   }
 });
